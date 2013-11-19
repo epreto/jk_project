dddad49ce5ab387176dc5fa3c960d042dd22f8d9
epreto <edmpreto@gmail.com>
2013-11-19 18:39:47 +0000

    Home

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JkProject::Application.config.secret_key_base = 'a07917b2f3cb833ca35b89f1f737d01bfbbfb46b4d0bede0bb2d607f1baaf6e8ad3b01aa2a405e98e1b79482b531a895d48a5241ad3a77792dc7bc722b1cd2d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JkProject::Application.config.session_store :cookie_store, key: '_jk_project_session'
